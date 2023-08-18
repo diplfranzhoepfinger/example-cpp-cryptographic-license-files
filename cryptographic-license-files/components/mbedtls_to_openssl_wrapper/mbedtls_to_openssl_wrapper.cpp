9156f2da60ca57d5daac10c8a923665a64d1c9c4
Franz Höpfinger <mail@franzhoepfinger.de>
2023-08-18 10:19:57 +0200

    malloc

--- cryptographic-license-files/components/mbedtls_to_openssl_wrapper/mbedtls_to_openssl_wrapper.cpp
+++ cryptographic-license-files/components/mbedtls_to_openssl_wrapper/mbedtls_to_openssl_wrapper.cpp
@@ -62,11 +62,18 @@ void EVP_CIPHER_CTX_free(EVP_CIPHER_CTX *ctx) {
 int EVP_CIPHER_CTX_ctrl(EVP_CIPHER_CTX *ctx, int type, int arg, void *ptr) {
     if (ctx && type == EVP_CTRL_GCM_SET_IVLEN) {
         ctx->iv_len = arg;
+        ctx->tag = (unsigned char*)malloc(ctx->iv_len);
     } else if (ctx && type == EVP_CTRL_GCM_GET_TAG) {
         unsigned char *tag = (unsigned char*)ptr;
         
         for (int i = 0; i < arg; i++) {
             tag[i] = ctx->tag[i];
+        }
+    } else if (ctx && type == EVP_CTRL_GCM_SET_TAG) {
+        unsigned char *tag = (unsigned char*)ptr;
+
+        for (int i = 0; i < arg; i++) {
+            ctx->tag[i] = tag[i];
         }
     }
     return 1;
@@ -107,7 +114,9 @@ int EVP_DecryptInit_ex(EVP_CIPHER_CTX *ctx, const EVP_CIPHER *cipher, ENGINE *im
 
 int EVP_DecryptUpdate(EVP_CIPHER_CTX *ctx, unsigned char *out, int *outl, const unsigned char *in, int inl) {
     if (ctx) {
-        ctx->tag = (unsigned char*)malloc(ctx->iv_len);
+        if (ctx->tag == NULL) {
+        	ctx->tag = (unsigned char*)malloc(ctx->iv_len);
+        }
         
         mbedtls_gcm_crypt_and_tag(&ctx->ctx, MBEDTLS_GCM_DECRYPT, inl, ctx->iv, ctx->iv_len, NULL, 0, in, out, ctx->iv_len, ctx->tag);
         *outl = inl;
