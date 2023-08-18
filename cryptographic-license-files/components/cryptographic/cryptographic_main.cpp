964746cc238926c91cff06ca10d20eb2bbc0b87a
Franz Höpfinger <mail@franzhoepfinger.de>
2023-08-18 09:40:51 +0200

    so far ..

--- cryptographic-license-files/components/cryptographic/cryptographic_main.cpp
+++ cryptographic-license-files/components/cryptographic/cryptographic_main.cpp
@@ -2,7 +2,9 @@
 #include "include/sha256/sha256.h"
 #include "include/base64/base64.h"
 #include "include/json/picojson.h"
-
+#include "openssl/rand.h"
+#include "openssl/evp.h"
+#include "mbedtls/gcm.h"
 #include <stdlib.h>
 #include <assert.h>
 #include <unordered_map>
